b022767a89c3e4553648279769e3844fc42b56c9
Delyan Stoichkov <delyan.stoichkov@egt-digital.com>
2025-05-19 14:10:55 +0300

    fix: remove env

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,5 @@
 import { App } from "./system/App";
 import { Config } from "./game/Config";
-import environment from "../utils/Environment";
-// Initialize environment handlers
 document.addEventListener('DOMContentLoaded', () => {
-    // Ensure environment is initialized before running the app
-    console.log('Device type:', environment.getDeviceType());
-    console.log('In iframe:', environment.inIframe());
-    
-    // Run the app
     App.run(Config);
 });
